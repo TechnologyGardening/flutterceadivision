05b4d6f1f75d0787cc6265e17e9061354e00502e
Technology Gardening <33565151+TechnologyGardening@users.noreply.github.com>
2025-07-22 08:37:03 +0530

    a1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutterceadivision/1507flutter/columnexperiment.dart';
 import 'package:flutterceadivision/1507flutter/containerexperiment.dart';
+import 'package:flutterceadivision/2207fluttwer/calculatorview.dart';
 
 void main() {
   runApp(const MainApp());
@@ -12,15 +13,15 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: ColumnExperiment(),
-      // home: CustomContainer(),
-      // home: Scaffold(
-      //   body: CustomContainer(),
-      //   // body: Center(
-      //   //   child: Text('3456789'),
-      //   // ),
-      // ),
-    );
+        debugShowCheckedModeBanner: false, home: CalculatorPart()
+        // home: ColumnExperiment(),
+        // home: CustomContainer(),
+        // home: Scaffold(
+        //   body: CustomContainer(),
+        //   // body: Center(
+        //   //   child: Text('3456789'),
+        //   // ),
+        // ),
+        );
   }
 }
